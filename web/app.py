d50094acfb45e594fc9ec3afeff98548e15ed86f
eshakusuma <89148463+eshakusuma@users.noreply.github.com>
2025-06-29 23:24:52 -0600

    Update app.py

--- web/app.py
+++ web/app.py
@@ -3,16 +3,12 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return send_file('where.html')
-
-@app.route('/')
-def index():
-    return send_from_directory('', 'where.html')
+def serve_home():
+    return send_from_directory('.', 'where.html')
 
 @app.route('/where.js')
-def where_js():
-    return send_from_directory('', 'where.js')
+def serve_js():
+    return send_from_directory('.', 'where.js')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run()
