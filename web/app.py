7f5b85fcc6b4c71f19c8b0b20c9bfb9c72ef7dd4
eshakusuma <89148463+eshakusuma@users.noreply.github.com>
2025-06-29 23:42:00 -0600

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,14 +1,11 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def serve_home():
-    return send_from_directory('.', 'where.html')
+    return app.send_static_file('where.html')
 
 @app.route('/where.js')
 def serve_js():
-    return send_from_directory('.', 'where.js')
-
-if __name__ == '__main__':
-    app.run()
+    return app.send_static_file('where.js')
